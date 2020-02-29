ef8297c24cfe56c40886cc2cb363dcee9b88678d
Dinesh Nanda <dannynanda76@gmail.com>
2020-02-29 17:10:26 -0500

    duplicate elements in array

---
+++ Duplicate in array~/DuplicateArray/src/main/java/duplicate/DuplicateArray.java
@@ -0,0 +1,26 @@
+
+package duplicate;
+
+/**
+ *
+ * @author Dinesh Nanda
+ */
+
+public class DuplicateArray {
+
+    public static void main(String[] args) {
+        
+        int arr[] = {1, 2, 3, 4, 2, 7, 8, 8, 3};
+
+        System.out.println("Duplicate elements in array are: ");
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.print(arr[j]+ " ");
+                }
+            }
+        }
+    } 
+}
